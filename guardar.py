6ce6acc54f0e7d1fa9da0391c2518c259d9875f7
Jesussanta <64745720+Jesussanta@users.noreply.github.com>
2021-10-16 14:41:04 -0500

    Create guardar.py

---
+++ guardar.py
@@ -0,0 +1,25 @@
+#Guarda encuestas
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import mysql.connector
+
+for i in range():
+    D1=input("Diagnostico(1): ")
+    D2=input("Diagnostico(2): ")
+    D3=input("Diagnostico(3): ")
+    UC1=input("Usailidas y calidad (1): ")
+    UC2=input("Usailidas y calidad (1): ")
+    UC3=input("Usailidas y calidad (1): ")
+    UC4=input("Usailidas y calidad (1): ")
+    UC5=input("Usailidas y calidad (1): ")
+    UC6=input("Usailidas y calidad (1): ")
+    UC7=input("Usailidas y calidad (1): ")
+
+data=[D1,D2,D3,UC1,UC2,UC3,UC4,UC5,UC6,UC7]
+
+print("Rectifique:")
+print(data)
+a = input("\nSubir datos?")
+if(a == "s" | a== "si" | a=="Si"):
+    
