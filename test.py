3fd90b1775df6b41116c077ecc0a52107606a66f
Jesussanta <64745720+Jesussanta@users.noreply.github.com>
2021-11-19 09:33:44 -0500

    1

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-import sys
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import mysql.connector
-from datetime import datetime
+person= []
+n = [2484,2839,4491,5492,5802,8862,9876,10970]
 
-
-8 - 1
-25 - 
-
-
+for t in range (len (n)):
+    l=[]
+    z=n[t]
+    for i in range(7):        
+        x=z+i*31
+        l.append(x)
+    person.append(l)
+    print(l)
 
